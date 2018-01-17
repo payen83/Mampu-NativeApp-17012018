26a970fc6b39789d200c55ee648a226988a0cf37
Ferdy Fauzi <payen83@gmail.com>
2018-01-18 00:57:33 +0800

    add map into homepage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+declare var google: any;
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
 
   constructor(public navCtrl: NavController) {
+
+  }
+
+  ionViewDidLoad(){
+    this.initMap();
+  }
+
+  initMap(){
+
+    let center = new google.maps.LatLng(2.9993472, 101.6783672);
+    let mapOptions = {
+      center: center,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      disableDefaultUI: true
+    }
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
 
   }
 
